d1ff0c8dd5fedfc385e9039c48f9d00a69075eaa
Nicholas Klein Castillanes <kleincastillanes07@gmail.com>
2025-12-10 14:52:25 +0800

    Implement text manipulation functions
    Added functions to replace spaces and count characters in text inputs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+/*
+    Author: Nicholas Klein Y. Castillanes || A221
+    Description: A compliance for Week 3 assesment in IT103
+*/
+
+function replaceSpace() {
+    let str1 = document.getElementById("replaceTxtInput").value;
+    
+    let newStr1 = str1.replaceAll(' ','');
+    document.getElementById("replaceTxtResult").innerHTML = newStr1;
+}
+
+function countCharacters() {
+    let str2 = document.getElementById("countTxtInput").value;
+    str2 = str2.trim();
+
+    let newStr2 = str2.length;
+    document.getElementById("countTxtResult").innerHTML = newStr2;
+}
+        
 
